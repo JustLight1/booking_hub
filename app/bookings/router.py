22e5fe271d867970f1af1172411ac1010734998e
Форов Александр <5h9t5@list.ru>
2024-01-22 20:23:53 +0300

    add new method <add> for bookings

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,8 +1,11 @@
+from datetime import date
 from fastapi import APIRouter, Depends
 
 from users.dependencies import get_current_user
 from users.models import Users
 from .dao import BookingDAO
+from .schemas import SBooking
+from exceptions import RoomCannotBeBooked
 
 router = APIRouter(
     prefix='/bookings',
@@ -11,6 +14,18 @@ router = APIRouter(
 
 
 @router.get('')
-async def get_bookings(user: Users = Depends(get_current_user)):
+async def get_bookings(
+    user: Users = Depends(get_current_user)
+) -> list[SBooking]:
     result = await BookingDAO.find_all(user_id=user.id)
     return result
+
+
+@router.post('')
+async def add_booking(
+    room_id: int, date_from: date, date_to: date,
+    user: Users = Depends(get_current_user)
+):
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBooked
--- app/main.py
+++ app/main.py
@@ -37,17 +37,6 @@ def get_hotels(
     return search_args
 
 
-class SBooking(BaseModel):
-    roob_id: int
-    date_from: date
-    date_to: date
-
-
-@app.post('/bookings')
-def add_booking(booking: SBooking):
-    pass
-
-
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info',
                 reload=True)
