f29e28b2af54f22b91e2eb5808ce33415c68dec8
Форов Александр <5h9t5@list.ru>
2024-09-11 18:46:40 +0300

    refactor(hotels/rooms/models): update annotates

--- app/hotels/rooms/models.py
+++ app/hotels/rooms/models.py
@@ -1,23 +1,29 @@
-from sqlalchemy import JSON, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from typing import TYPE_CHECKING
 
-from database import Base
+from sqlalchemy import JSON, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from app.database import Base
+
+if TYPE_CHECKING:
+    from app.hotels.models import Hotels
+    from app.bookings.models import Bookings
 
 
 class Rooms(Base):
-    __tablename__ = "rooms"
+    hotel_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey('hotels.id'), nullable=False
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    services: Mapped[dict] = mapped_column(JSON, nullable=True)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    image_id: Mapped[int] = mapped_column(Integer)
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    hotel_id = Column(ForeignKey("hotels.id"), nullable=False)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    price = Column(Integer, nullable=False)
-    services = Column(JSON, nullable=True)
-    quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
-
-    hotel = relationship("Hotels", back_populates="rooms")
-    booking = relationship("Bookings", back_populates="room")
+    hotel: Mapped['Hotels'] = relationship('Hotels', back_populates='rooms')
+    booking: Mapped[list['Bookings']] = relationship(
+        'Bookings', back_populates='room')
 
     def __str__(self):
-        return f"Номер {self.name}"
+        return f'Номер {self.name}'
