bd2357643fcbb716839e7ecc5a312579c964c3f9
Форов Александр <5h9t5@list.ru>
2024-09-11 17:19:22 +0300

    feat(database): update class Base and async_session_maker, add get_async_session

--- app/database.py
+++ app/database.py
@@ -1,15 +1,38 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import Integer
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import (DeclarativeBase, declared_attr, Mapped,
+                            mapped_column)
 
-from config import settings
+from app.config import settings
+
+
+class Base(DeclarativeBase):
+    """
+    Базовый класс для всех моделей.
+
+    Attributes:
+        __tablename__ (str): Имя таблицы, устанавливается как имя класса в
+        нижнем регистре.
+        id (Mapped[int]): Первичный ключ.
+    """
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
 
 engine = create_async_engine(settings.DATABASE_URL)
 
-async_session_maker = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session_maker = async_sessionmaker(engine,  expire_on_commit=False)
 
 
-class Base(DeclarativeBase):
-    pass
+async def get_async_session():
+    """
+    Создает асинхронную сессию для работы с базой данных.
+
+    Returns:
+        async_session_maker: Асинхронная сессия SQLAlchemy.
+    """
+    async with async_session_maker() as async_session:
+        yield async_session
