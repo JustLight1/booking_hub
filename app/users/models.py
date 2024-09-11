8a4d884f33d22e8e8a131753322dfa0909657d67
Форов Александр <5h9t5@list.ru>
2024-09-11 18:48:06 +0300

    refactor(users/models): update annotates

--- app/users/models.py
+++ app/users/models.py
@@ -1,10 +1,9 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
 
 
 class Users(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
