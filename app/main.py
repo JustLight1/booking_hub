9127f9985b5571af49fe37df22afa69652f545d6
Форов Александр <5h9t5@list.ru>
2024-09-14 17:30:30 +0300

    refactor(main): update imports

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
-from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from datetime import date
-from pydantic import BaseModel
-import uvicorn
+from fastapi import FastAPI
 
-from bookings.router import router as router_bookings
-from users.router import router as router_users
-from hotels.router import router as router_hotels
-from hotels.rooms.router import router as router_rooms
+from app.bookings.router import router as router_bookings
+from app.users.router import router as router_users
+from app.hotels.router import router as router_hotels
+from app.hotels.rooms.router import router as router_rooms
 
 app = FastAPI()
 
@@ -15,8 +11,3 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info',
-                reload=True)
