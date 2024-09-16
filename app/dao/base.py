39c26cd6a476b1a3c184fb3588d22b2a24c3ef18
Форов Александр <5h9t5@list.ru>
2024-09-16 18:11:19 +0300

    feat(dao/base): add typing

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,12 +1,18 @@
+from typing import Generic, Type, TypeVar
+
 from sqlalchemy import delete, select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import Base
+
+
+ModelType = TypeVar('ModelType', bound=Base)
 
 
 class BaseDAO:
-    model = None
+    model: Type[ModelType] = None
 
     @classmethod
-    async def find_by_id(cls, model_id: int, session: AsyncSession):
+    async def find_by_id(cls, session: AsyncSession, model_id: int):
         query = select(cls.model).filter_by(id=model_id)
         result = await session.execute(query)
         return result.scalar_one_or_none()
