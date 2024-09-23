c6f1978d5960c67253a8161221bd3d881421ba49
Форов Александр <5h9t5@list.ru>
2024-09-23 17:08:15 +0300

    feat(hotels/dao): upd imports and add session: AsyncSession

--- app/hotels/dao.py
+++ app/hotels/dao.py
@@ -1,30 +1,37 @@
 from datetime import date
 
-from sqlalchemy import func, insert, select, and_, or_
+from sqlalchemy import select, and_, or_, func
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from dao.base import BaseDAO
-from bookings.models import Bookings
-from hotels.models import Hotels
-from hotels.rooms.models import Rooms
-from database import async_session_maker
+from app.dao.base import BaseDAO
+from app.hotels.models import Hotels
+from app.hotels.rooms.models import Rooms
+from app.bookings.models import Bookings
 
 
 class HotelsDAO(BaseDAO):
     model = Hotels
 
     @classmethod
-    async def find_all(cls, location: str, date_from: date, date_to: date):
+    async def find_all(
+        cls,
+        location: str,
+        date_from: date,
+        date_to: date,
+        session: AsyncSession
+    ):
         """
         WITH booked_rooms AS (
             SELECT room_id, COUNT(room_id) AS rooms_booked
             FROM bookings
-            WHERE 
+            WHERE
                 (date_from >= '2023-05-15' AND date_from <= '2023-06-20') OR
                 (date_from <= '2023-05-15' AND date_to > '2023-05-15')
-            GROUP BY room_id
+           GROUP BY room_id
         ),
         booked_hotels AS (
-            SELECT hotel_id, SUM(rooms.quantity - COALESCE(rooms_booked, 0)) AS rooms_left
+            SELECT hotel_id,
+            SUM(rooms.quantity - COALESCE(rooms_booked, 0)) AS rooms_left
             FROM rooms
             LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
             GROUP BY hotel_id
@@ -33,10 +40,13 @@ class HotelsDAO(BaseDAO):
         LEFT JOIN booked_hotels ON booked_hotels.hotel_id = hotels.id
         WHERE rooms_left > 0 AND location LIKE '%Алтай%';
         """
+
         booked_rooms = (
-            select(Bookings.room_id, func.count(Bookings.room_id).label("rooms_booked"))
-            .select_from(Bookings)
-            .where(
+            select(
+                Bookings.room_id, func.count(Bookings.room_id).label(
+                    'rooms_booked'
+                )
+            ).select_from(Bookings).where(
                 or_(
                     and_(
                         Bookings.date_from >= date_from,
@@ -47,19 +57,21 @@ class HotelsDAO(BaseDAO):
                         Bookings.date_to > date_from,
                     ),
                 ),
-            )
-            .group_by(Bookings.room_id)
-            .cte("booked_rooms")
+            ).group_by(Bookings.room_id).cte('booked_rooms')
         )
 
         booked_hotels = (
-            select(Rooms.hotel_id, func.sum(
-                    Rooms.quantity - func.coalesce(booked_rooms.c.rooms_booked, 0)
-            ).label("rooms_left"))
-            .select_from(Rooms)
-            .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
-            .group_by(Rooms.hotel_id)
-            .cte("booked_hotels")
+            select(
+                Rooms.hotel_id,
+                func.sum(
+                    Rooms.quantity - func.coalesce(
+                        booked_rooms.c.rooms_booked, 0
+                    )
+                ).label('rooms_left')
+            ).select_from(Rooms)
+            .join(
+                booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True
+            ).group_by(Rooms.hotel_id).cte('booked_hotels')
         )
 
         get_hotels_with_rooms = (
@@ -68,16 +80,19 @@ class HotelsDAO(BaseDAO):
             #     Hotels
             #     booked_hotels.c.rooms_left,
             # )
-            # Но используется конструкция Hotels.__table__.columns. Почему? Таким образом алхимия отдает
-            # все столбцы по одному, как отдельный атрибут. Если передать всю модель Hotels и
-            # один дополнительный столбец rooms_left, то будет проблематично для Pydantic распарсить
-            # такую структуру данных. То есть проблема кроется именно в парсинге ответа алхимии 
-            # Пайдентиком.
+            # Но используется конструкция Hotels.__table__.columns. Почему?
+            # Таким образом алхимия отдает все столбцы по одному, как отдельный
+            # атрибут. Если передать всю модель Hotels и один дополнительный
+            # столбец rooms_left, то будет проблематично для Pydantic
+            # распарсить такую структуру данных. То есть проблема кроется
+            # именно в парсинге ответа алхимии Пайдентиком.
             select(
                 Hotels.__table__.columns,
                 booked_hotels.c.rooms_left,
             )
-            .join(booked_hotels, booked_hotels.c.hotel_id == Hotels.id, isouter=True)
+            .join(booked_hotels,
+                  booked_hotels.c.hotel_id == Hotels.id,
+                  isouter=True)
             .where(
                 and_(
                     booked_hotels.c.rooms_left > 0,
@@ -85,7 +100,6 @@ class HotelsDAO(BaseDAO):
                 )
             )
         )
-        async with async_session_maker() as session:
-            # logger.debug(get_hotels_with_rooms.compile(engine, compile_kwargs={"literal_binds": True}))
-            hotels_with_rooms = await session.execute(get_hotels_with_rooms)
-            return hotels_with_rooms.mappings().all()
+
+        hotels_with_rooms = await session.execute(get_hotels_with_rooms)
+        return hotels_with_rooms.mappings().all()
