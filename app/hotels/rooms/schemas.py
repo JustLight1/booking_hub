b0b5685a40e809604aae13589c464d73cfee1bf4
Форов Александр <5h9t5@list.ru>
2024-09-23 17:50:17 +0300

    refactor(hotels/rooms/schemas): add     model_config = ConfigDict(from_attributes=True) and upd typing

--- app/hotels/rooms/schemas.py
+++ app/hotels/rooms/schemas.py
@@ -1,25 +1,19 @@
-from typing import List, Optional
-
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class SRoom(BaseModel):
     id: int
     hotel_id: int
     name: str
-    description: Optional[str]
-    services: List[str]
+    description: str | None
+    services: list[str]
     price: int
     quantity: int
     image_id: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SRoomInfo(SRoom):
     total_cost: int
     rooms_left: int
-
-    class Config:
-        orm_mode = True
