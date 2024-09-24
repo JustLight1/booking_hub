8233824f8e80187cca9d7b896b120ffe6db13991
Форов Александр <5h9t5@list.ru>
2024-09-24 14:30:47 +0300

    feat(main): add cors and middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
@@ -18,3 +19,16 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_pages)
 app.include_router(router_images)
+
+origins = [
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT'],
+    allow_headers=['Content-Type', 'Set-Cookie', 'Access-Control-Allow-Origin',
+                   'Access-Control-Allow-Headers', 'Authorization']
+)
