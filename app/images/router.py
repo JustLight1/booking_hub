cbca5dc61e016eb0336c5a73058b7039492fc4a9
Форов Александр <5h9t5@list.ru>
2024-09-27 19:58:11 +0300

    feat(images/router): add celery task

--- app/images/router.py
+++ app/images/router.py
@@ -2,6 +2,8 @@ import shutil
 
 from fastapi import APIRouter, UploadFile
 
+from app.tasks.tasks import process_picture
+
 
 router = APIRouter(
     prefix='/images',
@@ -11,5 +13,7 @@ router = APIRouter(
 
 @router.post('/hotels')
 async def add_hotel_image(name: int, file: UploadFile):
-    with open(f'app/static/images/{name}.webp', 'wb+') as file_object:
+    image_path = f'app/static/images/{name}.webp'
+    with open(image_path, 'wb+') as file_object:
         shutil.copyfileobj(file.file, file_object)
+    process_picture.delay(image_path)
