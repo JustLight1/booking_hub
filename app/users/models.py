33de013e6d5e2d6bfe1022ffb31f8442a62d0653
Форов Александр <5h9t5@list.ru>
2024-09-28 20:24:45 +0300

    feat(users/models): add relationship

--- app/users/models.py
+++ app/users/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -7,3 +7,7 @@ from app.database import Base
 class Users(Base):
     email: Mapped[str] = mapped_column(String, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    booking = relationship('Bookings', back_populates='user')
+
+    def __str__(self) -> str:
+        return f'User {self.email}'
