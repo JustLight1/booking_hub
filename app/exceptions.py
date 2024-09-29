5bbbe073300e42af6213ba3e00c0f1c826d50603
Форов Александр <5h9t5@list.ru>
2024-09-29 18:29:23 +0300

    style(exceptions): change " " -> ' '

--- app/exceptions.py
+++ app/exceptions.py
@@ -45,12 +45,12 @@ class RoomCannotBeBooked(BookingException):
 
 class DateFromCannotBeAfterDateTo(BookingException):
     status_code = status.HTTP_400_BAD_REQUEST
-    detail = "Дата заезда не может быть позже даты выезда"
+    detail = 'Дата заезда не может быть позже даты выезда'
 
 
 class CannotBookHotelForLongPeriod(BookingException):
     status_code = status.HTTP_400_BAD_REQUEST
-    detail = "Невозможно забронировать отель сроком более месяца"
+    detail = 'Невозможно забронировать отель сроком более месяца'
 
 
 class CannotDeleteBooking(BookingException):
