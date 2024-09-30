7a13cc0335e562f6a993cddb12879e4977605d15
Форов Александр <5h9t5@list.ru>
2024-09-30 21:32:02 +0300

    feat(config): add test db and mode

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
+from typing import Literal
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal['DEV', 'TEST', 'PROD']
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -21,6 +24,19 @@ class Settings(BaseSettings):
         return (
             f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@'
             f'{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        )
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    @property
+    def TEST_DATABASE_URL(self):
+        return (
+            f'postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@'
+            f'{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}'
         )
 
     SECRET_KEY: str
